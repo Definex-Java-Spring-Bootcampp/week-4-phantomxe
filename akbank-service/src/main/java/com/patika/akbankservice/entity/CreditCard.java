43722292298353e6c1fcff5f84894d9dfdff550f
Hakan Afat <phantom3ee@gmail.com>
2024-04-08 12:07:12 +0300

    database integration

--- akbank-service/src/main/java/com/patika/akbankservice/entity/CreditCard.java
+++ akbank-service/src/main/java/com/patika/akbankservice/entity/CreditCard.java
@@ -3,15 +3,41 @@ package com.patika.akbankservice.entity;
 import java.math.BigDecimal;
 import java.util.List;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.RequiredArgsConstructor;
 import lombok.Setter;
+import lombok.ToString;
+
+import java.io.Serializable;
 
 @Getter
 @Setter
-@RequiredArgsConstructor
-public class CreditCard implements Product {
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+@Builder
+@Entity
+@Table(name = "application")
+public class CreditCard implements Serializable, Product {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "fee")
     private BigDecimal fee;
+
+    
     private List<Campaign> campaignList;
+
     private final String bank = "akbank";
 }
--- akbank-service/src/main/java/com/patika/akbankservice/entity/LoanLimit.java
+++ akbank-service/src/main/java/com/patika/akbankservice/entity/LoanLimit.java
@@ -2,16 +2,34 @@ package com.patika.akbankservice.entity;
 
 import java.math.BigDecimal;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
 
+import java.io.Serializable;
+
 @Getter
 @Setter
 @AllArgsConstructor
-public class LoanLimit {
+public class LoanLimit implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    
+    @Column(name = "max_amount")
     private BigDecimal maxAmount;
+    
+    @Column(name = "min_amount")
     private BigDecimal minAmount;
+
+    @Column(name = "interest_rate")
     private Double interestRate;
+
+    @Column(name = "installment")
     private Integer installment;
 }
